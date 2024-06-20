e52c3eb28b1ba6479c6c5cfa1daed1ef395883e1
teddy cubaka <teddycubaka96@gmail.com>
2024-06-20 18:27:09 +0100

    add heruko config file

--- app/api/dashboard.py
+++ app/api/dashboard.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from ..models import User, Agent, Schedule
+from ..models import User, Agent, Schedule, Localisation
 from .users import Response
 from .utils import Utils
 from flask_pymongo import pymongo, ObjectId
@@ -58,7 +58,7 @@ def schedule_registrement():
                     Response(
                         {
                             "code": 400,
-                            "message": "une erreur s'est produite lors de la création du compte",
+                            "message": "une erreur s'est produite lors de la création de l'horraire",
                             "error": modelSchedule.error,
                         }
                     ),
@@ -66,12 +66,7 @@ def schedule_registrement():
                 )
             return (
                 Response(
-                    {
-                        "code": 201,
-                        "message": "horraire ajouté",
-                        "data": schedule_data,
-                        # "actual_hour": now.strftime("%H:%M %p"),
-                    }
+                    {"code": 201, "message": "horraire ajouté", "data": schedule_data}
                 ),
                 201,
             )
@@ -125,7 +120,131 @@ def schedule_registrement():
             }
 
 
-@dashboard_router.route("/point/arrive", methods=["GET"])
+import datetime
+
+
+def is_between(start_time, end_time):
+    current_time = datetime.datetime.now()
+    now = current_time.strftime("%H:%M %p")
+
+    start_time_obj = datetime.datetime.strptime(start_time, "%H:%M %p")
+    end_time_obj = datetime.datetime.strptime(end_time, "%H:%M %p")
+
+    start_time_timestamp = start_time_obj.timestamp()
+    end_time_timestamp = end_time_obj.timestamp()
+
+    # Convert timestamps to milliseconds and round to nearest integer
+    start_time_ms = int(round(start_time_timestamp * 1000))
+    end_time_ms = int(round(end_time_timestamp * 1000))
+
+    return [start_time_ms, now, end_time_ms]
+    if start_time_obj <= now and now <= end_time_obj:
+        return True
+    else:
+        return False
+
+
+@dashboard_router.route("/point", methods=["GET", "POST", "PUT", "DELETE"])
 def pointArrive():
-    model = Schedule()
-    return Response({"code": 200, "data": model.find({"action": "ARRIVE"})}), 200
+    try:
+        model = Localisation()
+        match request.method:
+            case "GET":
+                allowed_filters = ["action", "agent"]
+                filters = {}
+                for key, value in request.args.items():
+                    # if key == "location.lat":
+                    #     filters["location"]["lat"] = float(value)
+                    # if key == "location.lng":
+                    #     filters["location"]["lng"] = float(value)
+                    if key in allowed_filters:
+                        if key == "_id":
+                            try:
+                                filters[key] = ObjectId(value)
+                            except (ValueError, TypeError):
+                                pass
+                        else:
+                            filters[key] = value
+
+                return (
+                    Response(
+                        {"code": 200, "data": model.find(filters), "filter": filters}
+                    ),
+                    200,
+                )
+            case "POST":
+                utils = Utils()
+                data = request.get_json()
+                isDataValid = utils.dataValidator(
+                    data,
+                    ["location", "action", "agent"],
+                )
+
+                if isDataValid == False:
+                    return {
+                        "code": 400,
+                        "message": "certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : start, end, action",
+                        "error": utils.error,
+                    }, 400
+
+                isDataValid = utils.dataValidator(
+                    data["location"],
+                    [
+                        "lat",
+                        "lng",
+                    ],
+                )
+                if isDataValid == False:
+                    return {
+                        "code": 400,
+                        "message": "certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : start, end, action",
+                        "error": utils.error,
+                    }, 400
+                scheduleModel = Schedule
+                horraire = scheduleModel.find({"action": data["action"]})
+                is_late = is_between(horraire[0].get("start"), horraire[0].get("end"))
+
+                data["is_late"] = is_late
+
+                model = Localisation(data)
+                model.save()
+                if model.error is not None:
+                    return (
+                        Response(
+                            {
+                                "code": 400,
+                                "message": "une erreur s'est produite lors du pointage",
+                                "error": model.error,
+                            }
+                        ),
+                        400,
+                    )
+                return (
+                    Response(
+                        {
+                            "code": 201,
+                            "message": "pointé effectué avec succès",
+                            "data": data,
+                            "horraire": horraire[0],
+                        }
+                    ),
+                    201,
+                )
+    except TypeError as error:
+        return {
+            "code": 400,
+            "message": "une erreur s'est produite.",
+            "details": str(error),
+        }
+    except ValueError as error:
+        return {
+            "code": 400,
+            "message": "une erreur s'est produite.",
+            "details": str(error),
+        }
+    except KeyError as error:
+        return {
+            "code": 400,
+            "message": "une erreur s'est produite.",
+            "details": str(error),
+        }
--- app/api/users.py
+++ app/api/users.py
@@ -1,4 +1,4 @@
-from ..models import User, Agent
+from ..models import User, Agent, Localisation
 from flask import Blueprint, request, jsonify
 from flask_pymongo import pymongo
 from bson import json_util, ObjectId
@@ -12,11 +12,10 @@ def hash_pwd(pwd):
     salt = gensalt(13)
     pwd = pwd.encode("utf-8")
     bytes_data = hashpw(pwd, salt)
-    return bytes_data.decode('utf-8')
+    return bytes_data.decode("utf-8")
 
 
-users_router = Blueprint(
-    'users_router', 'users_router', url_prefix='/api/v1/user')
+users_router = Blueprint("users_router", "users_router", url_prefix="/api/v1/user")
 
 
 class JSONEncoder(json.JSONEncoder):
@@ -33,261 +32,284 @@ class JSONEncoder(json.JSONEncoder):
             return json.JSONEncoder.default(self, obj)
         # return json_util.default(obj, json_util.CANONICAL_JSON_OPTIONS)
 
-def Response(data) :
+
+def Response(data):
     return json.loads(JSONEncoder().encode(data))
 
 
-@users_router.route('/', methods=["GET"])
+@users_router.route("/", methods=["GET"])
 def get_all_users():
     try:
         userModel = User()
         documents = userModel.find()
-        return Response({
-            "code": 200,
-            "users": documents
-        }), 200
+        return Response({"code": 200, "users": documents}), 200
 
     except pymongo.errors.PyMongoError as e:
-        return jsonify({
-            'error': 'An error occurred while retrieving users.',
-            'details': str(e)
-        }), 500
+        return (
+            jsonify(
+                {
+                    "error": "An error occurred while retrieving users.",
+                    "details": str(e),
+                }
+            ),
+            500,
+        )
     except Exception as e:
-        return jsonify({
-            'error': 'An error occurred while retrieving users.',
-            'details': str(e)
-        }), 500
+        return (
+            jsonify(
+                {
+                    "error": "An error occurred while retrieving users.",
+                    "details": str(e),
+                }
+            ),
+            500,
+        )
 
 
-@users_router.route('/agent/signup', methods=['GET', 'POST'])
+@users_router.route("/agent/signup", methods=["GET", "POST"])
 def save_agent():
     try:
         utils = Utils()
         data = request.get_json()
         isDataValid = utils.dataValidator(
-            data, ['username', 'mobile_no', 'pwd', 'pwd_repeat', "username", "pwd", "is_root", "mobile_no", "pwd_repeat", "firstname", "lastname", "grade", "poste"])
+            data,
+            [
+                "username",
+                "mobile_no",
+                "pwd",
+                "pwd_repeat",
+                "username",
+                "pwd",
+                "is_root",
+                "mobile_no",
+                "pwd_repeat",
+                "firstname",
+                "lastname",
+                "grade",
+                "poste",
+            ],
+        )
 
         if isDataValid == False:
             return {
-                'code': 400,
-                'message': 'certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : username, mobile_no, pwd, pwd_repeat',
-                'error': utils.error
+                "code": 400,
+                "message": "certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : username, mobile_no, pwd, pwd_repeat",
+                "error": utils.error,
             }, 400
-        if utils.passwordVerify(data['pwd'], data['pwd_repeat']) == False:
+        if utils.passwordVerify(data["pwd"], data["pwd_repeat"]) == False:
             return {
-                'code': 400,
-                'message': utils.error['details'],
-                'error': utils.error
+                "code": 400,
+                "message": utils.error["details"],
+                "error": utils.error,
             }, 400
 
         user_data = {
-            "username": data['username'],
-            "pwd": data['pwd'],
-            "is_root": data['is_root'] if data['is_root'] is not None else False,
-            "mobile_no": data['mobile_no']
+            "username": data["username"],
+            "pwd": data["pwd"],
+            "is_root": data["is_root"] if data["is_root"] is not None else False,
+            "mobile_no": data["mobile_no"],
         }
 
-        user_data['pwd'] = hash_pwd(user_data['pwd'])
+        user_data["pwd"] = hash_pwd(user_data["pwd"])
         userModel = User(user_data)
-        existed_user = userModel.find({"username": data['username']})
+        existed_user = userModel.find({"username": data["username"]})
         if len(existed_user) > 0:
             return {
                 "code": 400,
                 "message": "l'utilisateur avec ce nom existe déjà. Veuillez choisir un autre nom d'utilisateur",
             }
-        existed_user = userModel.find({"mobile_no": data['mobile_no']})
+        existed_user = userModel.find({"mobile_no": data["mobile_no"]})
         if len(existed_user) > 0:
             return {
                 "code": 400,
-                "message": "Ce numéro de téléphone existe déjà dans le système. Veuillez choisir un autre nom d'utilisateur"
+                "message": "Ce numéro de téléphone existe déjà dans le système. Veuillez choisir un autre nom d'utilisateur",
             }
         userModel.save()
         if userModel.error is not None:
-            return Response({
-                'code': 400,
-                'message': 'une erreur s\'est produite lors de la création du compte',
-                'error': userModel.error
-            }), 400
+            return (
+                Response(
+                    {
+                        "code": 400,
+                        "message": "une erreur s'est produite lors de la création du compte",
+                        "error": userModel.error,
+                    }
+                ),
+                400,
+            )
 
         agentData = {
-            "firstname": data['firstname'],
-            "lastname": data['lastname'],
-            "grade": data['grade'],
-            "poste": data['poste'],
-            "as_user": user_data['_id']
+            "firstname": data["firstname"],
+            "lastname": data["lastname"],
+            "grade": data["grade"],
+            "poste": data["poste"],
+            "as_user": user_data["_id"],
         }
 
         agentModel = Agent(agentData)
         agentModel.save()
         if agentModel.error is not None:
             userModel.delete()
-            return Response({
-                'code': 400,
-                'message': 'une erreur s\'est produite lors de la création du compte',
-                'error': agentModel.error
-            }), 400
+            return (
+                Response(
+                    {
+                        "code": 400,
+                        "message": "une erreur s'est produite lors de la création du compte",
+                        "error": agentModel.error,
+                    }
+                ),
+                400,
+            )
+        agentData["as_user"] = user_data
 
-        agentData['as_user'] = user_data
-
-        return Response({
-            "code": 200,
-            "data": agentData
-        })
+        return Response({"code": 200, "data": agentData})
     except pymongo.errors.PyMongoError as e:
-        return jsonify({
-            'error': 'user not created',
-            'details': str(e)
-        }), 400
+        return jsonify({"error": "user not created", "details": str(e)}), 400
     except Exception as e:
-        return jsonify({
-            'error': 'user not created',
-            'details': str(e)
-        }), 400
+        return jsonify({"error": "user not created", "details": str(e)}), 400
     except TypeError as error:
-        return jsonify({
-            'error': 'user not created',
-            'details': str(error)
-        }), 400
+        return jsonify({"error": "user not created", "details": str(error)}), 400
 
 
-@users_router.route('/agent/login', methods=["POST"])
+@users_router.route("/agent/login", methods=["POST"])
 def user_login():
     try:
         utils = Utils()
         data = request.get_json()
-        isDataValid = utils.dataValidator(data, ['login', 'pwd'])
+        isDataValid = utils.dataValidator(data, ["login", "pwd"])
         if isDataValid == False:
             return {
-                'code': 400,
-                'message': 'certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : login, pwd',
-                'error': utils.error
+                "code": 400,
+                "message": "certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : login, pwd",
+                "error": utils.error,
             }, 400
         userModel = User()
-        if isinstance(data['login'], (int, float, complex)):
+        if isinstance(data["login"], (int, float, complex)):
             loginStr = "numéro de téléphone"
-            users = userModel.find({"mobile_no": data['login']})
+            users = userModel.find({"mobile_no": data["login"]})
         else:
             loginStr = "nom d'utilisateur"
-            users = userModel.find({"username": data['login']})
+            users = userModel.find({"username": data["login"]})
 
         if users is None or len(users) == 0:
-            return {
-                "code": 404,
-                "message": f"{loginStr} ou mot de passe incorrect"
-            }
+            return {"code": 404, "message": f"{loginStr} ou mot de passe incorrect"}
 
         user = users[0]
-        password = data['pwd'].encode('utf-8')
-        hashed_password = user['pwd'].encode('utf-8')
+        password = data["pwd"].encode("utf-8")
+        hashed_password = user["pwd"].encode("utf-8")
         if checkpw(password=password, hashed_password=hashed_password) is not True:
             return {
                 "code": 404,
-                "message": f"{loginStr} ou mot de passe incorrect"
+                "message": f"{loginStr} ou mot de passe incorrect",
             }, 404
 
         agentModel = Agent()
-        agent = agentModel.find({"as_user": user['_id']})
+        agent = agentModel.find({"as_user": user["_id"]})
         if agent is None or len(agent) == 0:
-            return {
-                "code": 404,
-                "message": f"{loginStr} ou mot de passe incorrect"
-            }
+            return {"code": 404, "message": f"{loginStr} ou mot de passe incorrect"}
 
         agent = agent[0]
+        pointing = Localisation().find({"agent": agent.get("_id")})
 
         agent["as_user"] = user
+        agent["pointing"] = pointing
 
-        return Response({
-            "code": 200,
-            "message": f"bienvenue {agent['firstname']}",
-            "data": agent
-        }), 200
+        return (
+            Response(
+                {
+                    "code": 200,
+                    "message": f"bienvenue {agent['firstname']}",
+                    "data": agent,
+                }
+            ),
+            200,
+        )
 
     except TypeError as error:
-        return jsonify({
-            'error': 'échec de connexion',
-            'details': str(error),
-            'type': "TypeError"
-        }), 400
+        return (
+            jsonify(
+                {
+                    "error": "échec de connexion",
+                    "details": str(error),
+                    "type": "TypeError",
+                }
+            ),
+            400,
+        )
     except pymongo.errors.PyMongoError as e:
-        return jsonify({
-            'error': 'échec de connexion',
-            'details': str(e)
-        }), 400
+        return jsonify({"error": "échec de connexion", "details": str(e)}), 400
     except Exception as error:
-        return jsonify({
-            'error': 'échec de connexion',
-            'details': str(error),
-            'type': "Exception"
-        }), 400
+        return (
+            jsonify(
+                {
+                    "error": "échec de connexion",
+                    "details": str(error),
+                    "type": "Exception",
+                }
+            ),
+            400,
+        )
 
 
-@users_router.route('/super_user', methods=['GET', 'POST'])
+@users_router.route("/super_user", methods=["GET", "POST"])
 def save_user():
     try:
         utils = Utils()
         data = request.get_json()
         isDataValid = utils.dataValidator(
-            data, ['pwd_repeat', "username", "pwd", "is_root", "mobile_no"])
+            data, ["pwd_repeat", "username", "pwd", "is_root", "mobile_no"]
+        )
 
         if isDataValid == False:
             return {
-                'code': 400,
-                'message': 'certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : username, mobile_no, pwd, pwd_repeat',
-                'error': utils.error
+                "code": 400,
+                "message": "certaines données sont manquante. Vérifiez si vous envoyer toutes ces clé : username, mobile_no, pwd, pwd_repeat",
+                "error": utils.error,
             }, 400
-        if utils.passwordVerify(data['pwd'], data['pwd_repeat']) == False:
+        if utils.passwordVerify(data["pwd"], data["pwd_repeat"]) == False:
             return {
-                'code': 400,
-                'message': utils.error['details'],
-                'error': utils.error
+                "code": 400,
+                "message": utils.error["details"],
+                "error": utils.error,
             }, 400
 
         user_data = {
-            "username": data['username'],
-            "pwd": data['pwd'],
-            "is_root": data['is_root'] if data['is_root'] is not None else False,
-            "mobile_no": data['mobile_no']
+            "username": data["username"],
+            "pwd": data["pwd"],
+            "is_root": data["is_root"] if data["is_root"] is not None else False,
+            "mobile_no": data["mobile_no"],
         }
 
-        user_data['pwd'] = hash_pwd(user_data['pwd'])
+        user_data["pwd"] = hash_pwd(user_data["pwd"])
         userModel = User(user_data)
-        existed_user = userModel.find({"username": data['username']})
+        existed_user = userModel.find({"username": data["username"]})
         if len(existed_user) > 0:
             return {
                 "code": 400,
                 "message": "l'utilisateur avec ce nom existe déjà. Veuillez choisir un autre nom d'utilisateur",
             }
-        existed_user = userModel.find({"mobile_no": data['mobile_no']})
+        existed_user = userModel.find({"mobile_no": data["mobile_no"]})
         if len(existed_user) > 0:
             return {
                 "code": 400,
-                "message": "Ce numéro de téléphone existe déjà dans le système. Veuillez choisir un autre nom d'utilisateur"
+                "message": "Ce numéro de téléphone existe déjà dans le système. Veuillez choisir un autre nom d'utilisateur",
             }
         userModel.save()
         if userModel.error is not None:
-            return Response({
-                'code': 400,
-                'message': 'une erreur s\'est produite lors de la création du compte',
-                'error': userModel.error
-            }), 400
+            return (
+                Response(
+                    {
+                        "code": 400,
+                        "message": "une erreur s'est produite lors de la création du compte",
+                        "error": userModel.error,
+                    }
+                ),
+                400,
+            )
 
-        return Response({
-            "code": 200,
-            "data": user_data
-        })
+        return Response({"code": 200, "data": user_data})
     except pymongo.errors.PyMongoError as e:
-        return jsonify({
-            'error': 'user not created',
-            'details': str(e)
-        }), 400
+        return jsonify({"error": "user not created", "details": str(e)}), 400
     except Exception as e:
-        return jsonify({
-            'error': 'user not created',
-            'details': str(e)
-        }), 400
+        return jsonify({"error": "user not created", "details": str(e)}), 400
     except TypeError as error:
-        return jsonify({
-            'error': 'user not created',
-            'details': str(error)
-        }), 400
+        return jsonify({"error": "user not created", "details": str(error)}), 400
